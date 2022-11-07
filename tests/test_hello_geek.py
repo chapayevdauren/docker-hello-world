a8dd1249479589120ed14918d5b3f90fe9d1e8b2
Chapaev Dauren <chapaevdauren@gmail.com>
2022-11-07 20:13:23 +0600

    Test CI tests

--- tests/test_hello_geek.py
+++ tests/test_hello_geek.py
@@ -4,3 +4,7 @@ from src.app import hello_geek
 def test_hello_geek():
     result = hello_geek()
     assert result == '<h1>Hello from Flask & Docker</h2>'
+
+
+def test_nothing():
+    assert True == False
